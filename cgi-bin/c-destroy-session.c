bc9e9ea29d236b15b84a580f9359e0ebe56a947d
Leon Tan <leontann29@gmail.com>
2023-04-30 20:27:26 +0000

    reformat C

--- cgi-bin/c-destroy-session.c
+++ cgi-bin/c-destroy-session.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv, char **envp)
-{
+int main(int argc, char **argv, char **envp) {
   // Headers
   printf("Cache-Control: no-cache\n");
   printf("Set-Cookie: destroyed\n");
--- cgi-bin/c-post-echo.c
+++ cgi-bin/c-post-echo.c
@@ -1,8 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int main(int argc, char **argv, char **envp)
-{
+int main(int argc, char **argv, char **envp) {
   char str[1000];
   // Print HTML header
   printf("Cache-Control: no-cache\n");
