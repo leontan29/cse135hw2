f74008f2c0b9a471f49702f8e8c7960621bc0857
Leon Tan <leontann29@gmail.com>
2023-05-02 21:37:22 +0000

    minor

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -x
 set -e
-#rsync -av collector.cse135byleon.site /var/www
-#rsync -av reporting.cse135byleon.site /var/www
+rsync -av collector.cse135byleon.site /var/www
+rsync -av reporting.cse135byleon.site /var/www
 (cd cgi-bin && make)
 rsync -av html /var/www
 rsync -av cgi-bin /var/www
