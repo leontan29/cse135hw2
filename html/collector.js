d2ba66dd6bba8e21552dd9fac07df1b7c11be77d
Leon Tan <leontann29@gmail.com>
2023-05-25 21:56:28 +0000

    wip

--- html/collector.js
+++ html/collector.js
@@ -1,6 +1,59 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+	let c = ca[i];
+	while (c.charAt(0) == ' ') {
+	    c = c.substring(1);
+	}
+	if (c.indexOf(name) == 0) {
+	    return c.substring(name.length, c.length);
+	}
+    }
+    return "";
+}
+
+function setCookie(name, value, days) {
+  var expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + days);
+
+  var cookieValue = encodeURIComponent(value) + '; expires=' + expirationDate.toUTCString() + '; path=/';
+  document.cookie = name + '=' + cookieValue;
+}
+
+function generateRandomString(length) {
+  var charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var randomString = '';
+
+  if (window.crypto && window.crypto.getRandomValues) {
+    var values = new Uint32Array(length);
+    window.crypto.getRandomValues(values);
+    for (var i = 0; i < length; i++) {
+      randomString += charset[values[i] % charset.length];
+    }
+  } else {
+    for (var i = 0; i < length; i++) {
+      randomString += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+  }
+
+  return randomString;
+}
+
+
+const TRACKERNAME = 'cse135tracker';
+let tracker = getCookie(TRACKERNAME);
+if (!tracker) {
+    tracker = '' + Date().getTime() + ':' + generateRandomString(10);
+    setCookie(TRACKERNAME, tracker, 1);
+}
+
 
 // send a packet of data to endpoint
 function send(data, endpoint) {
+    // always include the tracker and the page path
+    data['tracker'] = tracker;
     data['page'] = window.location.pathname;
     json = JSON.stringify(data);
     var params = new URLSearchParams();
@@ -19,7 +72,7 @@ function sendStaticData() {
 	screen_width: screen.width,
 	window_height: window.innerHeight,
 	window_width: window.innerWidth,
-	network_type: navigator.connection.type,
+	network_type: navigator.connection.effectiveType,
     };
     send(data, '/api/static');
 }
@@ -51,11 +104,12 @@ let activity_accum = [
 // if the activity_accum[] is not empty, send content to
 // to server, and empty it.
 function sendActivityData(force) {
-    if (!force && activity_accum.length < 10) {
+    if (!force && activity_accum.length < 50) {
 	// avoid sending too frequently
 	return;
     }
-    
+
+    // take out all activities in the accum
     let activities = activity_accum;
     activity_accum = [];
 
@@ -64,14 +118,17 @@ function sendActivityData(force) {
 	return;
     }
 
+    // make a data msg
     var data = {};
     for (var item in activities) {
 	const [typ, value] = item;
 	data[typ] = value;
     }
+    // send it
     send(data, '/api/activity');
 }
 
+// ----------------------------------------
 // keeps track of idle-time
 var idle_time = 0;
 function reset_idle_time() {
@@ -82,6 +139,15 @@ function reset_idle_time() {
 	idle_time = 0;
     }
 }
+
+// check idle time every 100ms
+setInterval(() => {
+    idle_time += 100;
+    // force send every 5 second
+    force = (idle_time % 5000 == 0);
+    sendActivityData(force);
+}, 100);
+
 
 document.addEventListener('mousemove', (e) => {
     reset_idle_time();
@@ -109,16 +175,26 @@ document.addEventListener('scroll', (e) => {
     sendActivityData(false);
 });
 
-// on page unload, send all accumulated activity data
+// ------------------------------
+// collect errors
+window.onerror = (msg, src, line, col, error) => {
+    let str = `${msg} (${src}:${line})`;
+    activity_accum.push(['error', str]);
+    sendActivityData(false);
+}
+
+
+// ------------------------------
+// user done with page: send all accumulated activity data
+document.addEventListener('visibilitychange', () => {
+    if (document.visibilityState == 'hidden') {
+	reset_idle_time();
+	sendActivityData(true);
+    }
+});
+
 document.addEventListener('unload', (e) => {
-    reset_idle_time();
     activity_accum.push(['leave_page_at', Date.now()]);
     sendActivityData(true);
 });
-
-// check idle time every 100ms
-setInterval(() => {
-    idle_time += 100;
-    sendActivityData(false);
-}, 100);
 
